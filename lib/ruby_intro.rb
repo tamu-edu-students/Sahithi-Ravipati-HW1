b89c4cc082620a85526d39b152f517e5ebeb97a1
sahithi <sahithi.ravipati@yahoo.com>
2022-09-10 19:32:52 -0500

    Completing starter code and adding log file

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,34 +3,85 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  sum = arr.sum {|v| v} 
+  return sum
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  end 
+  return arr.max(2).sum
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  for i in 0...arr.size 
+    for j in i+1...arr.size
+      if arr.at(i) + arr.at(j) == number
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+name
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+  if /^[^aeiouAEIOU\W]/i.match(string) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+  if string.match?(/[^01]/) or string.empty?
+    return false
+  end
+
+  num = string.to_i(2)
+
+  if num%4 == 0
+    return true
+  else 
+    return false
+  end 
+
 end
 
 # Part 3
 
 # Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  
+  def initialize(isbn,price)
+    raise ArgumentError if price<=0 or ! (isbn.is_a? String) or isbn.empty?
+    @isbn = isbn
+    @price = price
+  end
+
+  def isbn
+    @isbn
+  end
+
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+
+  def price
+    @price
+  end
+
+  def price=(price)
+    @price = price
+  end
+
+  def price_as_string
+    "$%0.2f" % [@price]
+  end
 end
